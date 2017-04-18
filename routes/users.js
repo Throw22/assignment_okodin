c49d4b273d9316a2af825b2096d78f5af22e5c29
Nicholas Romeo <romeonicholas@gmail.com>
2017-04-18 15:48:40 -0700

    Finish profile update route

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ let router = express.Router();
 var models = require('./../models');
 var User = models.User;
 var Profile = models.Profile;
+var Location = models.Location;
 var sequelize = models.sequelize;
 
 var helpers = require('./../helpers');
@@ -42,37 +43,67 @@ var onEdit = (req, res) => {
 };
 
 var onUpdate = (req, res) => {
-  console.log(req.body);
-  sequelize.transaction(t => {
-    return profile.update({
-      {"profile": {
-    "age": "44",
-    "height": "68",
-    "children": "2",
-    "pets": "7 cats and 8 dogs",
-    "occupation": "Supervisor",
-    "talents": [
-      "Ameliorated incremental implementation",
-      "Business-focused national intranet",
-      "I'll override the mobile SMS hard drive, that should protocol the SSL hard drive!",
-      "",
-      ""
-    ],
-    "favorites": [
-      "Perspiciatis eaque libero asperiores.",
-      "Nemo impedit voluptatum quia earum qui.",
-      "Voluptas asperiores quia laudantium ex fuga consectetur.",
-      "Eius modi soluta nihil aspernatur in.",
-      "",
-      ""
-    ],
-    "whyMe": "rich leverage niches"
-  }
-
-}, { where: (userId : req.body.userId) });
+  let talents = req.body.profile.talents.filter(function(talent) {
+    return talent.length > 0;
   });
 
-  res.redirect('/');
+  let favorites = req.body.profile.favorites.filter(function(favorite) {
+    return favorite.length > 0;
+  });
+
+  let photo = 'viking_girl.jpg';
+  if (req.body.profile.gender == 'male') {
+    photo = 'viking_guy.jpg';
+  }
+
+  let maritalStatus = {
+    1: 'Single',
+    2: 'Married',
+    3: 'Dating',
+    4: "It's complicated",
+    5: 'Married to the game'
+  };
+
+  let bodyType = {
+    1: 'Built like a god',
+    2: 'Built like a fish',
+    3: 'Built like a ground squirrel',
+    4: 'Built like a weird trapezoid',
+    5: 'Barely built at all'
+  };
+
+  sequelize
+    .transaction(t => {
+      return Location.findOrCreate({
+        defaults: { city: req.body.location.city },
+        where: { city: req.body.location.city },
+        transaction: t
+      }).spread(location => {
+        return Profile.update(
+          {
+            age: req.body.profile.age,
+            gender: req.body.profile.gender,
+            maritalStatus: maritalStatus[req.body.profile.maritalStatus],
+            bodyType: bodyType[req.body.profile.bodyType],
+            about: req.body.profile.about,
+            height: req.body.profile.height,
+            children: req.body.profile.children,
+            pets: req.body.profile.pets,
+            occupation: req.body.profile.occupation,
+            talents: talents,
+            favorites: favorites,
+            whyMe: req.body.profile.whyMe,
+            photo: photo,
+            locationId: location.id
+          },
+          { where: { userId: req.body.userId } },
+          { transaction: t }
+        );
+      });
+    })
+    .then(() => {
+      res.redirect(`user/${req.body.userId}`);
+    });
 };
 
 router.get('/user/:id', onShow);
